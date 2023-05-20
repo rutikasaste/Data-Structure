94fae2aefe7a07ce17261fb628a2211ebce0e598
Rutika Saste <113428140+sasterutika2219@users.noreply.github.com>
2023-05-20 15:38:32 +0530

    Add files via upload

---
+++ TowerOfHanoi.c
@@ -0,0 +1,27 @@
+/*Tower of Hanoi :-
+Tower of Hanoi is a mathematical puzzle where we have three rods (A, B, and C) and N disks. Initially, all the disks are stacked in decreasing 
+value of diameter i.e., the smallest disk is placed on the top and they are on rod A. The objective of the puzzle is to move the entire stack to
+ another rod (here considered C), obeying the following simple rules: 
+
+Only one disk can be moved at a time.
+Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved 
+if it is the uppermost disk on a stack.
+No disk may be placed on top of a smaller disk.*/
+
+#include<stdio.h>
+
+void Tow(int N ,char from_rod , char via_rod , char to_rod){
+    if(N>0){
+        Tow(N-1 ,from_rod ,to_rod,via_rod);
+        printf("Move disk %d From %c -> %c\n" ,N,from_rod ,to_rod);
+        Tow(N-1 ,via_rod ,from_rod ,to_rod);
+    }
+
+}
+void main(){
+    int N;
+    printf("Enter the value of N :");
+    scanf("%d",&N);
+    Tow(N,'A','B','C');
+
+}
